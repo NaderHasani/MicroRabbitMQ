5737284b2dbe53ad64cf4906a8bb6077b327e442
NaderHasani <nader.hasani99@gmail.com>
2021-05-16 14:12:11 +0430

    intial Commit of Infrasturcture Dependancy Project

---
+++ MicroRabbitMQ/MicroRabbit.Infra.IOC/DependancyContainer.cs
@@ -0,0 +1,15 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace MicroRabbit.Infra.IOC
+{
+    public class DependancyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
